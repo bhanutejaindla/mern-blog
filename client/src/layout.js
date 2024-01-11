a8e63aaae20cb4900f8342d5210bd7647dad89b3
bhanutejaindla <bhanutejaindla123@gmail.com>
2024-01-11 10:58:55 +0530

    hi

---
+++ client/src/layout.js
@@ -0,0 +1,10 @@
+import Header from "./Header";
+import {Outlet} from "react-router-dom";
+export default function(){
+    return(
+          <main>
+            <Header />
+            <Outlet />
+          </main>
+    );
+}
